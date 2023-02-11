84e05484c666f2e68b21b3382c104b08b15408e9
gr8warrior <shailusuriyal@gmail.com>
2023-02-12 01:25:21 +0530

    Added windows PSK code

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             ElevatedButton(
-              onPressed: _configurePSK,
+              onPressed: _getBatteryLevel,
               child: const Text('Configure PSK'),
             ),
             Text(_batteryLevel),
